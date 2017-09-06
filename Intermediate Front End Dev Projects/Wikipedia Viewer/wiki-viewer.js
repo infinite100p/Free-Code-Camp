8f78bc225fcfe6ba4cdfa3d7e2d6a31444b928e2
Tina Su <luckyanime777@gmail.com>
2017-09-06 06:41:59 -0400

    Set limit on text length

--- Intermediate Front End Dev Projects/Wikipedia Viewer/wiki-viewer.js
+++ Intermediate Front End Dev Projects/Wikipedia Viewer/wiki-viewer.js
@@ -4,6 +4,9 @@
   * @version 1.0
 */
 
+const SENTENCE_LIMIT = 3;       // number of words to display
+
+
 /************************************* MAIN CODE *****************************************/
 
 getRandomWiki();
@@ -12,18 +15,25 @@ getRandomWiki();
 function getRandomWiki() {
   $("#btn-random").on("click", function() {
     getTitle().then(function(wikiTitle) {
+      // var url = `https://en.wikipedia.org/wiki/ ${wikiTitle}`;
+      var url = 'https://en.wikipedia.org/wiki/' + wikiTitle;
       $.getJSON(
         "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
           wikiTitle +
           "&origin=*&gsrsearch="
       ).then(function(wikiEntry) {
-        $("#random").html(wikiEntry[0] + " - " + wikiEntry[2]);
+        var title = wikiEntry[0];
+        var content = wikiEntry[2];
+        // $("#content").addClass('highlight');
+        $("#content").html( `<h4> ${title} </h4> ${clipContent(content)} <p> <a href=${url} target="_blank"> Read more...</a>`);
+        // $('#random').css('background-color', '#F5FBF2');
+        
       });
     });
   });
 }
 
-/** Get the title of a random wiki entry
+/** Parse JSON data, retrieve random wiki entry, and store its title for future reference
   * @return {Deferred Object} stores the title of the randomly selected wiki entry
 */
 function getTitle() {
@@ -41,6 +51,46 @@ function getTitle() {
   });
   return defer;
 }
+
+
+/**
+  * Clip the content and add "..." if its length > 20 words
+  * @return {String} the shortened content
+ */
+function clipContent(content) {
+    // var words = content.split(' '); // array of words
+    if (content.length > SENTENCE_LIMIT) {
+      return content.slice(0, SENTENCE_LIMIT).join(" ") + "...";
+    } else {
+      return arrToStr(content);
+    }
+}
+
+/**
+  * Convert array to string
+  * @return {String} string of words in array without commas
+ */
+function arrToStr(arr) {
+  return arr.join(" ").replace(",", " ");
+}
+
+console.log(clipContent("hey there mister, are you okay?"));
+      // console.log(typeof ["yo"].slice(0, SENTENCE_LIMIT + 1).join(","));
+
+
+/************************************* QUESTIONS *****************************************/
+
+
+/* Parameter name in clipContent() MUST be "content". Otherwise deferred error is fired. Why?
+
+
+  Can't interpolate function parameter, wikiTitle...?
+
+  var url = `https://en.wikipedia.org/wiki/ ${wikiTitle}`;
+
+
+*/
+
 
 /************************************* PREVIOUS CODE *****************************************/
 
